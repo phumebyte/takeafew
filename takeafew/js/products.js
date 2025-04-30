47ffeb0b7f07a0799ce83be95d07478eca152248
radebeneo <radebeneo17@gmail.com>
2025-04-30 13:40:55 +0200

    Fixing commit mistake

--- takeafew/js/products.js
+++ takeafew/js/products.js
@@ -56,8 +56,7 @@ function displayProducts(products) {
         const viewProductDialog = document.getElementById('view-product-dialog');
         const productDetailsModal = document.getElementById('product-detail');
 
-  
-      console.log(product)
+        
       let reviewsHTML = '<h3>Reviews</h3>'
 
       for (let i = 0; i < product.reviews.length; i++) {
