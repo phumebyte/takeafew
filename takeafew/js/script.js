782ccc35c1d83fb64512b174fd793d360f8fc9ed
Phume Dlamini <phumelele.dlamini1000@gmail.com>
2025-05-02 11:32:02 +0200

    Implement user registration and login functionality

--- takeafew/js/script.js
+++ takeafew/js/script.js
@@ -53,5 +53,63 @@ const closeCartDialog = document.getElementById('cart-close-btn')
     viewRegisterDialog.close()
   })
 
-  const wishlistToggle = document.getElementById('wishlist-toggle')
-  wishlistToggle.onclick = function() {toggleWishlist()}
+  // const wishlistToggle = document.getElementById('wishlist-toggle')
+  // wishlistToggle.onclick = function() {toggleWishlist()}
+
+  const submitRegister = document.getElementById('submit-register')
+
+  submitRegister.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    registerUser()
+  })
+
+  function registerUser(){
+    const firstName = document.getElementById('name').value
+    const lastName = document.getElementById('lastName').value
+    const email = document.getElementById('register-email').value
+    const cellphone = document.getElementById('cellphone').value
+    const password = document.getElementById('register-password').value
+    const confirmedPassword = document.getElementById('confirmed-password').value
+
+    if(!email || !password){
+      alert('Please enter all fields')
+      return
+    }
+
+    console.log(firstName + " " + lastName + " " + email + " " + password + " " + cellphone) 
+
+    if(firstName && lastName && email && email && cellphone && password && confirmedPassword){
+      localStorage.setItem("user", JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password
+      }))
+
+      viewRegisterDialog.close()
+    }
+  }
+
+  const submitLogin = document.getElementById('submit-login')
+  
+  submitLogin.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    loginUser()
+  })
+
+  function loginUser(){
+    const email = document.getElementById('login-email').value
+    const password = document.getElementById('login-password').value
+
+    let user = JSON.parse(localStorage.getItem('user'))
+
+    if(user.email === email && user.password){
+      localStorage.setItem('loggedIn', true)
+      alert('User successfully logged in')
+      viewLoginDialog.close()
+    } else {
+      alert('Something went wrong')
+    }
+  }
